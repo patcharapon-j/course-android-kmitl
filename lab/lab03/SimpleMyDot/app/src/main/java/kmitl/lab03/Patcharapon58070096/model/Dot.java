c3f4cffd1a334b8dd21e1aca81e9e528821de136
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-08-25 11:25:42 +0700

    generate constrauctor and getter setter

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/Patcharapon58070096/model/Dot.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/Patcharapon58070096/model/Dot.java
@@ -1,13 +1,38 @@
 package kmitl.lab03.Patcharapon58070096.model;
 
-/**
- * Created by patcharaponjoksamut on 25/8/2017 AD.
- */
-
 public class Dot {
 
     private int centerX;
     private int centerY;
-    protected int radius;
+    private int radius;
 
+    public Dot(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
+
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 }

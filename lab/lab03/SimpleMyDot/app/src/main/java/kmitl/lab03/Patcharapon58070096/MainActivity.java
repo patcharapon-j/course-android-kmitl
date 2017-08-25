ea22097fcc0565aa034e3bd44b48e4f18c6aad99
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-08-25 11:21:09 +0700

    implement postion randomization

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/Patcharapon58070096/MainActivity.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/Patcharapon58070096/MainActivity.java
@@ -2,7 +2,13 @@ package kmitl.lab03.Patcharapon58070096;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onRandomDotClicked(View view) {
-        
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int screenWidth = displayMetrics.widthPixels;
+        int screenHeight = displayMetrics.heightPixels;
+
+        Random random = new Random();
+        int centerX = random.nextInt(screenWidth);
+        int centerY = random.nextInt(screenHeight);
+
+        String posLabelString = "X: " + centerX + "\nY: " + centerY;
+
+        TextView posLabel = (TextView)findViewById(R.id.posLabel);
+        posLabel.setText(posLabelString);
+
     }
 }

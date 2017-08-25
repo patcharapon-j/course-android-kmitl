c7fdc6b8a628f5b0ab4703e191b6c57a02e12032
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-08-25 11:21:21 +0700

    create Dot model class

---
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/Patcharapon58070096/model/Dot.java
@@ -0,0 +1,13 @@
+package kmitl.lab03.Patcharapon58070096.model;
+
+/**
+ * Created by patcharaponjoksamut on 25/8/2017 AD.
+ */
+
+public class Dot {
+
+    private int centerX;
+    private int centerY;
+    protected int radius;
+
+}
